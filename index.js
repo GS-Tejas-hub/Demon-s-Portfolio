f8499e627e4cda48376145a0e1dcd640ea93920e
G S Tejas <127507929+GS-Tejas-hub@users.noreply.github.com>
2024-06-17 18:39:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+
+let portfolio_btn = document.querySelector("#portfolio")
+
+portfolio_btn.addEventListener("click", () => {
+    let portfolio_section = document.querySelector("#portfolio-div");
+    console.log(portfolio_section);
+    portfolio_section.scrollIntoView()
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+let allAnchorTag = document.querySelectorAll("a")
+
+allAnchorTag.forEach((val) => {
+
+    console.log(val.classList.add("achor_underline"));
+});
+
+var typed = new Typed(".profession",{
+    strings:["Full Stack Web Developer","UI/UX Designer", "Photographer","Full Stack Web Developer","UI/UX Designer", "Photographer","Full Stack Web Developer","UI/UX Designer", "Photographer"],
+    typeSpeed:80,
+    backSpeed:10,
+    loop:true,
+    loopCount:Infinity,
+    showCursor:true,
+    cursorChar:"|"
+})
